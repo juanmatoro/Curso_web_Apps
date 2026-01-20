45daa48156dc620033d18de33355081dbb305293
JuanMa gToro <torito@hazmeweb.es>
2026-01-20 13:27:18 +0100

    deleted by us:   demo-js/exercises/exercice2.js

--- demo-js/exercises/exercice2.js
+++
@@ -1,6 +0,0 @@
-// 2 Declara una variable de las dos formas posibles, e imprime por consola los dos valores.
-console.log('Ejercicio 2');
-let numero1 = 10;
-const numero2 = 5;
-console.log(`${numero1} + ${numero2} = ${numero1 + numero2}`);
-
