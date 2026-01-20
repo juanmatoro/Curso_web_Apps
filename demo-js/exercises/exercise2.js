898a9b0343094b466681bf5ded67e59924395945
JuanMa gToro <torito@hazmeweb.es>
2026-01-20 13:25:31 +0100

    replace "exercice2.js" with "exercise2.js"

---
+++ demo-js/exercises/exercise2.js
@@ -0,0 +1,6 @@
+// 2 Declara una variable de las dos formas posibles, e imprime por consola los dos valores.
+console.log('Ejercicio 2');
+let numero1 = 10;
+const numero2 = 5;
+console.log(`${numero1} + ${numero2} = ${numero1 + numero2}`);
+
